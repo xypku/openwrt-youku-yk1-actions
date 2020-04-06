2b4d23d44867a5e12802b6a47393c925b885c5f5
xypku <63218155+xypku@users.noreply.github.com>
2020-04-06 15:34:05 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -8,3 +8,8 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
+git clone https://github.com/ujincn/luci-app-smartdns-compat.git package/luci-app-smartdns-compat
+git clone https://github.com/pymumu/smartdns.git package/smartdns
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+./scripts/feeds update -a
+./scripts/feeds install -a
