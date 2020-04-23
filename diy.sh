ee4344fa66d3645fee8ed8fea3e5fe0ff21ea09a
xypku <63218155+xypku@users.noreply.github.com>
2020-04-23 09:34:41 +0800

    add AppFilter

--- diy.sh
+++ diy.sh
@@ -10,8 +10,9 @@
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/ujincn/luci-app-smartdns-compat.git package/luci-app-smartdns-compat
 git clone https://github.com/pymumu/smartdns.git package/smartdns
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
-git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+#git clone https://github.com/jefferymvp/luci-app-koolproxyR package/luci-app-koolproxyR
 #git clone https://github.com/vernesong/OpenClash.git package/OpenClash
 ./scripts/feeds update -a
 ./scripts/feeds install -a
