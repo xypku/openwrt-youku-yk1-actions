4f073842c69976a2e219be5adf4cec30f7f07f91
xypku <63218155+xypku@users.noreply.github.com>
2020-07-03 12:41:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,13 +8,16 @@
 # 这里的脚本在获取feeds后执行
 #=================================================
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 #git clone https://github.com/kenzok8/openwrt-packages package/op-packages
 #=================================================
 #=================================================
 # 获取luci-app-adguardhome
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome package/diy-packages/luci-app-adguardhome
+git clone https://github.com/ujincn/luci-app-smartdns-compat.git package/luci-app-smartdns-compat
+git clone https://github.com/pymumu/smartdns.git package/smartdns
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 # 获取hello world和依赖
 git clone https://github.com/jerrykuku/lua-maxminddb package/diy-packages/helloworld/lua-maxminddb
 git clone https://github.com/jerrykuku/luci-app-vssr package/diy-packages/helloworld/luci-app-vssr
@@ -44,9 +47,9 @@ rm -rf luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon luci-theme-argon
 popd
 #=================================================
-#=================================================
-# 修改BaiduPCS-web来源
-pushd package/lean
-rm -rf baidupcs-web
-git clone https://github.com/liuran001/baidupcs-web-lede baidupcs-web
-popd
+
+
+# Modify target.mk
+cd include
+rm ./target.mk
+wget https://raw.githubusercontent.com/xypku/openwrt-youku-yk1-actions/master/target.mk
